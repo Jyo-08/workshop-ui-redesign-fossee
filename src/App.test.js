342f12289ca8475d2e6afe7b71f911115704a954
Jyo-08 <jyotish1628@gmail.com>
2026-04-09 00:26:42 +0530

    fix: correct App.test.js to import App instead of Login

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import Login from './Login';
+import App from './App';
 
-test('renders learn react link', () => {
+test('renders workshop booking heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
